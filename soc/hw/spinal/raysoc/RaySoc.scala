370cb25b6afa6ee2e5216c0785258d78af3389ff
Laurentiu Ciobanu <laurentiu@ciobanu.dev>
2024-10-26 09:49:00 +0000

    ray-soc: init spinalhdl

---
+++ soc/hw/spinal/raysoc/RaySoc.scala
@@ -0,0 +1,25 @@
+package raysoc
+
+import spinal.core._
+
+case class RaySoc() extends Component {
+  val io = new Bundle {
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
+    val state = out UInt(8 bits)
+  }
+
+  val counter = Reg(UInt(8 bits)) init 0
+
+  when(io.cond0) {
+    counter := counter + 1
+  }
+
+  io.state := counter
+  io.flag := (counter === 0) | io.cond1
+}
+
+object RaySocVerilog extends App {
+  Config.spinal.generateVerilog(RaySoc())
+}
